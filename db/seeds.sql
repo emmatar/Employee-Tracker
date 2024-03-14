bd18e386b112f34ad0da834fa149e437e80cf7de
Elijah Matar <m.matar515@gmail.com>
2024-03-14 16:47:55 -0500

    added more data to seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,18 +8,23 @@ VALUES ("Engineering"),
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 4),
-       ("Sales", 80000, 4),
+       ("Salesperson", 80000, 4),
        ("Lead Engineer", 150000, 1),
        ("Software Engineer", 120000, 1),
        ("Account Manager", 160000, 2),
        ("Accoutant", 125000, 2),
-       ("Legal Team Lead", 250000, 3);
 
-INSERT INTO employees (first_name, last_name)
-VALUES ("Mike", "Chan"),
-       ("Ashley", "Rodriguez"),
-       ("Kevin", "Tupik"),
-       ("Kunal", "Singh"),
-       ("Malia", "Brown"),
-       ("Sarah", "Lourd"),
-       ("Tom", "Allen");
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES 
+       ("Ashley", "Rodriguez", 4, 9),
+       ("Kunal", "Singh", 2, 8),
+       ("Malia", "Brown", 2, 8),
+       ("Sarah", "Lourd", 4, 9),
+       ("Tom", "Allen", 4, 9),
+       ("Ben", "Simmons", 6, 10),
+       ("Chauncey", "Billups", 2, 8),
+       ("Mike", "Chan", 1, 8),
+       ("Mr", "Bean", 3, 9),
+       ("Kevin", "Tupik", 5, 10);
+
