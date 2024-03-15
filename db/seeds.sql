3e164c2a0e9aafcb590bc3b2570b6fe7c72a4e34
Elijah Matar <m.matar515@gmail.com>
2024-03-14 22:09:02 -0500

    corrected seed structure for managers/employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,19 +12,21 @@ VALUES ("Sales Lead", 100000, 4),
        ("Lead Engineer", 150000, 1),
        ("Software Engineer", 120000, 1),
        ("Account Manager", 160000, 2),
-       ("Accoutant", 125000, 2),
+       ("Accoutant", 125000, 2);
 
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("Mike", "Chan", 1),
+       ("Mr", "Bean", 3),
+       ("Kevin", "Tupik", 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-       ("Ashley", "Rodriguez", 4, 9),
-       ("Kunal", "Singh", 2, 8),
-       ("Malia", "Brown", 2, 8),
-       ("Sarah", "Lourd", 4, 9),
-       ("Tom", "Allen", 4, 9),
-       ("Ben", "Simmons", 6, 10),
-       ("Chauncey", "Billups", 2, 8),
-       ("Mike", "Chan", 1, 8),
-       ("Mr", "Bean", 3, 9),
-       ("Kevin", "Tupik", 5, 10);
+       ("Ashley", "Rodriguez", 4, 1),
+       ("Kunal", "Singh", 2, 2),
+       ("Malia", "Brown", 2, 3),
+       ("Sarah", "Lourd", 4, 1),
+       ("Tom", "Allen", 4, 1),
+       ("Ben", "Simmons", 6, 2),
+       ("Chauncey", "Billups", 2, 3);
 
+-- match the employee manager_i
